467ce1adf18e18ec42d671342860ae9f4322ce0e
junminChang <chjm219@kookmin.ac.kr>
2020-07-09 14:26:35 +0900

    Collection and examples

---
+++ app/src/main/java/kotlin/14. Collection.kt
@@ -0,0 +1,53 @@
+// 14. Collection
+// -> list, set, map
+// List
+
+fun main(array: Array<String>) {
+
+    //Immutable collection(변경 및 추가 불)
+    //List -> 중복을 허용한다.
+    val numberList = listOf<Int>(1, 2, 3, 3)
+    println(numberList.get(0))
+    println(numberList[0])
+
+
+
+    // Set -> 중복을 허용하지 않는다.
+    // -> 순서가 없다!!! 즉 인덱스가 없
+    val numberSet = setOf<Int>(1, 2, 3, 3, 3)
+    println()
+    numberSet.forEach {
+        println(it)
+    }
+
+
+    // Map -> Key, Value 방식으로 관리한다
+    val numberMap = mapOf<String, Int>("one" to 1, "two" to 2)
+    println()
+    println(numberMap.get("one"))
+
+
+
+
+
+
+    // Mutabale Collection가(변경 및 추가 가능)
+    val mNumberList = mutableListOf<Int>(1, 2, 3)
+    mNumberList.add(3, 4)
+    println()
+    println(mNumberList)
+
+    val mNumberSet = mutableSetOf<Int>(1, 2, 3, 4, 4, 4)
+    mNumberSet.add(10)
+    println()
+    println(mNumberSet)
+
+    val mNumberMap = mutableMapOf<String, Int>("one" to 1)
+    mNumberMap.put("two", 2)
+    println()
+    println(mNumberMap)
+
+
+
+
+}
---
+++ app/src/main/java/kotlin/15. Practice.kt
@@ -0,0 +1,46 @@
+import android.os.Build
+import androidx.annotation.RequiresApi
+
+// 15. Practice
+
+
+@RequiresApi(Build.VERSION_CODES.N)
+fun main(array: Array<String>) {
+    val a = mutableListOf<Int>(1, 2, 3)
+    a.add(4)
+    println(a)
+    a.add(0, 100)
+    println(a)
+    a.set(0, 200)
+    println(a)
+    a.removeAt(1)
+    println(a)
+
+    val b = mutableSetOf<Int>(1, 2, 3, 4)
+    println()
+    b.add(2)
+    println(b)
+    b.remove(2)
+    println(b)
+    b.remove(100)
+    println(b)
+
+
+    val c = mutableMapOf<String, Int>("one" to 1)
+    println()
+    println(c)
+    c.put("two", 2)
+    println(c)
+    c.replace("two", 3)
+    println(c)
+    println()
+
+
+    println(c.keys)
+    println(c.values)
+
+    c.clear()
+    println(c)
+
+
+}
