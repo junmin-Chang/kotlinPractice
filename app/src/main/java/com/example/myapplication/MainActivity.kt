39ae50ba5d551262d93f0a4a5ca6cbeadb111b06
junminChang <chjm219@kookmin.ac.kr>
2020-07-17 23:57:35 +0900

    assignment and drawable

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.scrollview_13)
+        setContentView(R.layout.assignment_05)
     }
 }
