1dd9f4ea0eee02c25b108396d0946423f7db552d
junminChang <chjm219@kookmin.ac.kr>
2020-07-18 23:49:11 +0900

    import xml and send it to activity file

---
+++ app/src/main/java/com/example/myapplication/Listener.kt
@@ -0,0 +1,25 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_listener.*
+
+class Listener : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_listener)
+
+        // 뷰를 액티비티로 가져오는 방법
+        // 1) 직접 찾아서 가져온
+       // val textview : TextView = findViewById(R.id.hello)
+        // 2) xml을 import해서 가져온
+       //  hello
+
+
+
+
+
+
+    }
+}
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,39 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.assignment_05_re)
+        setContentView(R.layout.assignment_05)
+
+        Log.d("Life_Cycle", "onCreate")
+    }
+
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("Life_Cycle", "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("Life_Cycle", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("Life_Cycle", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("Life_Cycle", "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("Life_Cycle", "onDestroy")
     }
 }
