1ca17679025152873d6305a3f6a72eb39071a726
junminChang <chjm219@kookmin.ac.kr>
2020-07-21 20:02:49 +0900

    intent_3

--- app/src/main/java/com/example/myapplication/Intent1.kt
+++ app/src/main/java/com/example/myapplication/Intent1.kt
@@ -3,6 +3,7 @@ package com.example.myapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_intent.*
 import kotlinx.android.synthetic.main.activity_intent2.*
 
@@ -29,8 +30,24 @@ class Intent1 : AppCompatActivity() {
             intent2.apply {
                 this.putExtra("number1", 1)
                 this.putExtra("number2", 13)
-                startActivity(intent2)
+
             }
+            startActivityForResult(intent2, 200)
         }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+
+        if (requestCode == 200) {
+            Log.d("number", "" + requestCode)
+            Log.d("number", "" + resultCode)
+
+            val result = data?.getIntExtra("result", 0)
+            Log.d("number", "" + result)
+        }
+
+
+
+        super.onActivityResult(requestCode, resultCode, data)
     }
 }
