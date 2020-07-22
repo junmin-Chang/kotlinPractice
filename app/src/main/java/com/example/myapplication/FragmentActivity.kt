855ed7cd8670fbff7b0283cd388dfc3aee3fa069
junminChang <chjm219@kookmin.ac.kr>
2020-07-22 23:14:19 +0900

    fragment_2

--- app/src/main/java/com/example/myapplication/FragmentActivity.kt
+++ app/src/main/java/com/example/myapplication/FragmentActivity.kt
@@ -1,8 +1,10 @@
 package com.example.myapplication
 
+import FragmentOne
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import kotlinx.android.synthetic.main.activity_fragment.*
 
 class FragmentActivity : AppCompatActivity() {
 
@@ -11,6 +13,24 @@ class FragmentActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fragment)
         Log.d("Life_Cycle", "onCreate")
+
+
+        Button.setOnClickListener {
+            // 프라그먼트를 동적으로 작동하는 방법
+            val fragmentOne : FragmentOne = FragmentOne()
+            val fragmentManager = supportFragmentManager
+
+
+            // Transaction
+            // 작업의 단위 --> 시작과 끝이 있다
+            val fragmentTransaction = fragmentManager.beginTransaction()
+            fragmentTransaction.replace(R.id.container, fragmentOne)
+            fragmentTransaction.commit()
+
+            // 끝을 내는 방법
+            // commit   ->  시간 될 때 해(좀더 안정적)
+            // commitnow    -> 지금 당장해
+        }
     }
 
 
