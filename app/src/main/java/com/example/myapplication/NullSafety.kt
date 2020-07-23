3a142fd750770b1d4d47b5cf5beec659bd7c1510
junminChang <chjm219@kookmin.ac.kr>
2020-07-23 21:47:41 +0900

    resource

--- app/src/main/java/com/example/myapplication/NullSafety.kt
+++ app/src/main/java/com/example/myapplication/NullSafety.kt
@@ -21,26 +21,26 @@ class NullSafety : AppCompatActivity() {
         setContentView(R.layout.activity_null_safety)
 
 
-        val number: Int = 10
-        val number1: Int? = null
-
-        // !! -> 개발자가 null이 아님을 보장
-        val number5 : Int = number1!! + 10
-
-//        val number3  = number? + number1
-        val number3 = number1?.plus(number)
-       // Log.d("number", "number3: " + number3)
-
-
-        // 삼항연산자 -> 엘비스 연산자(?:)
-        // Null Safety를 위한 도구
-        val number4 = number1 ?: 10
-
-
-        //Log.d("number", "number4: " + number4)
-
-        lateCar = Car(10)
-        Log.d("number", "late number : " + lateCar.number)
+//        val number: Int = 10
+//        val number1: Int? = null
+//
+//        // !! -> 개발자가 null이 아님을 보장
+//        val number5 : Int = number1!! + 10
+//
+////        val number3  = number? + number1
+//        val number3 = number1?.plus(number)
+//       // Log.d("number", "number3: " + number3)
+//
+//
+//        // 삼항연산자 -> 엘비스 연산자(?:)
+//        // Null Safety를 위한 도구
+//        val number4 = number1 ?: 10
+//
+//
+//        //Log.d("number", "number4: " + number4)
+//
+//        lateCar = Car(10)
+//        Log.d("number", "late number : " + lateCar.number)
 
     }
 
