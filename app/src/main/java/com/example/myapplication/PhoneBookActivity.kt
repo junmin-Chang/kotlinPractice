1726f027a8bfb6923aa8406f27506575efe0aa73
junminChang <chjm219@kookmin.ac.kr>
2020-07-25 22:16:05 +0900

    addview_assignment

--- app/src/main/java/com/example/myapplication/PhoneBookActivity.kt
+++ app/src/main/java/com/example/myapplication/PhoneBookActivity.kt
@@ -1,8 +1,10 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.widget.LinearLayout
 import android.widget.TextView
 
@@ -28,7 +30,7 @@ class PhoneBookActivity : AppCompatActivity() {
 
 
 
-    
+
     fun createPhoneBookList(phoneBook: PhoneBook) {
         val layoutInflater = LayoutInflater.from(this@PhoneBookActivity)
         val container = findViewById<LinearLayout>(R.id.phonebook_list_container)
@@ -37,11 +39,23 @@ class PhoneBookActivity : AppCompatActivity() {
                 val view = layoutInflater.inflate(R.layout.people, null)
                val personNameView = view.findViewById<TextView>(R.id.person_name)
                 personNameView.setText(phoneBook.personList.get(i).name)
+
+                addSetOnClickListener(phoneBook.personList.get(i), view)
+
                 container.addView(view)
 
 
 
             }
+    }
+
+    fun addSetOnClickListener(person: Person, view: View) {
+        view.setOnClickListener {
+            val intent = Intent(this@PhoneBookActivity, PhoneBookDetailActivity::class.java)
+            intent.putExtra("name", person.name)
+            intent.putExtra("number", person.number)
+            startActivity(intent)
+        }
     }
 }
 
---
+++ app/src/main/java/com/example/myapplication/PhoneBookDetailActivity.kt
@@ -0,0 +1,33 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_phone_book_detail.*
+
+class PhoneBookDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_phone_book_detail)
+
+
+        getPersonInfoAndDraw()
+
+
+        back.setOnClickListener {
+            onBackPressed()
+        }
+
+    }
+
+
+
+
+
+    fun getPersonInfoAndDraw() {
+        val name = intent.getStringExtra("name")
+        val number = intent.getStringExtra("number")
+
+        person_detail_name.setText(name)
+        person_detail_number.setText(number)
+    }
+}
