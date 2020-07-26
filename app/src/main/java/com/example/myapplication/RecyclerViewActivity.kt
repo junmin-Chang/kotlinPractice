d92119f94b027e3b1577b303005b7881b15a23c2
junminChang <chjm219@kookmin.ac.kr>
2020-07-26 23:33:20 +0900

    recycler_view_2

--- app/src/main/java/com/example/myapplication/RecyclerViewActivity.kt
+++ app/src/main/java/com/example/myapplication/RecyclerViewActivity.kt
@@ -2,18 +2,23 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_recycler_view.*
 
 class RecyclerViewActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recycler_view)
+
 
 
 
@@ -27,7 +32,9 @@ class RecyclerViewActivity : AppCompatActivity() {
         val adapter = RecyclerViewAdapter(carlist, LayoutInflater.from(this@RecyclerViewActivity))
 
         recycler_view.adapter = adapter
-        recycler_view.layoutManager = LinearLayoutManager(this@RecyclerViewActivity)
+       recycler_view.layoutManager = LinearLayoutManager(this@RecyclerViewActivity)
+
+       // recycler_view.layoutManager = GridLayoutManager(this@RecyclerViewActivity, 2)
 
     }
 }
@@ -38,13 +45,22 @@ class RecyclerViewAdapter(
 
 ) : RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>() {
 
-    class ViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
+    inner class ViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
         val carName: TextView?
         val carEngine: TextView?
 
         init {
             carName = itemView.findViewById(R.id.car_name)
             carEngine = itemView.findViewById(R.id.car_engine)
+            itemView.setOnClickListener {
+
+                val position: Int = adapterPosition
+                val engineName = itemList.get(position).engine
+
+                Log.d("engine", engineName)
+
+
+            }
         }
 
     }
