4121cfd36bdf58e4fe2753e4317ec700ee5a3a31
junminChang <chjm219@kookmin.ac.kr>
2020-07-26 15:58:34 +0900

    recycler_view

--- app/src/main/java/com/example/myapplication/ListViewActivity.kt
+++ app/src/main/java/com/example/myapplication/ListViewActivity.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 import android.widget.TextView
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_list_view.*
 
 class ListViewActivity : AppCompatActivity() {
@@ -20,23 +21,43 @@ class ListViewActivity : AppCompatActivity() {
             carList.add(CarForList("" + i + "번째 car", "" + i + "순위 engine"))
         }
 
-        val adapter = ListViewAdapter(carList, this@ListViewActivity)
+        val adapter = ListViewAdapter(carList, this@ListViewActivity, layoutInflater)
 
 
         listView.adapter = adapter
 
+        listView.setOnItemClickListener { parent, view, p0, id ->
+            val carName = (adapter.getItem(p0) as CarForList).name
+            val carEngine = (adapter.getItem(p0) as CarForList).engine
+
+            Toast.makeText(this@ListViewActivity, carName + "" + carEngine, Toast.LENGTH_SHORT).show()
+        }
+
+
     }
 }
-class ListViewAdapter(val carForList: ArrayList<CarForList>, val context: Context) : BaseAdapter() {
+
+class ListViewAdapter(val carForList: ArrayList<CarForList>, val context: Context, val layoutInflater: LayoutInflater) : BaseAdapter() {
     override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
-        val layoutInflater = LayoutInflater.from(context)
-        val view = layoutInflater.inflate(R.layout.item_view, null)
-        val carNameTextview = view.findViewById<TextView>(R.id.car_name)
-        val carEngineTextview = view.findViewById<TextView>(R.id.car_engine)
 
+        val view: View
+        val holder: ViewHolder
 
-        carEngineTextview.setText(carForList.get(p0).engine)
-        carNameTextview.setText(carForList.get(p0).name)
+        if (p1 == null) {
+            view = layoutInflater.inflate(R.layout.item_view, null)
+            holder = ViewHolder()
+
+            holder.carName = view.findViewById(R.id.car_name)
+            holder.carEngine = view.findViewById(R.id.car_engine)
+
+            view.tag = holder
+        } else {
+            holder = p1.tag as ViewHolder
+            view = p1
+        }
+
+        holder.carName?.setText(carForList.get(p0).name)
+        holder.carEngine?.setText(carForList.get(p0).engine)
 
 
         return view
@@ -45,7 +66,7 @@ class ListViewAdapter(val carForList: ArrayList<CarForList>, val context: Contex
 
     override fun getItem(p0: Int): Any {
         //그리고자 하는 아이템 리스트의 하나(p0에 해당하)
-       return carForList.get(p0)
+        return carForList.get(p0)
     }
 
     override fun getItemId(p0: Int): Long {
@@ -57,4 +78,10 @@ class ListViewAdapter(val carForList: ArrayList<CarForList>, val context: Contex
         //그리고자 하는 아이템 리스트의 전체 갯수
         return carForList.size
     }
+}
+
+
+class ViewHolder() {
+    var carName: TextView? = null
+    var carEngine: TextView? = null
 }
---
+++ app/src/main/java/com/example/myapplication/RecyclerViewActivity.kt
@@ -0,0 +1,64 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_recycler_view.*
+
+class RecyclerViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recycler_view)
+
+
+
+
+        val carlist = ArrayList<CarForList>()
+        for (i in 0 until 50) {
+
+            carlist.add(CarForList("" + i + " 번째 car", "" + i + "순위 engine"))
+        }
+
+        val adapter = RecyclerViewAdapter(carlist, LayoutInflater.from(this@RecyclerViewActivity))
+
+        recycler_view.adapter = adapter
+        recycler_view.layoutManager = LinearLayoutManager(this@RecyclerViewActivity)
+
+    }
+}
+
+class RecyclerViewAdapter(
+    val itemList: ArrayList<CarForList>,
+    val inflater: LayoutInflater
+
+) : RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>() {
+    class ViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
+        val carName: TextView?
+        val carEngine: TextView?
+
+        init {
+            carName = itemView.findViewById(R.id.car_name)
+            carEngine = itemView.findViewById(R.id.car_engine)
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = inflater.inflate(R.layout.item_view, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return itemList.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.carName?.setText(itemList.get(position).name)
+        holder.carEngine?.setText(itemList.get(position).engine)
+    }
+}
